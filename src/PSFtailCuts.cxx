4492875f4bc26182a266dca2db0b547bba892a39
usher <>
2001-10-31 17:04:08 +0000

    This version has the energy/angle dependent tangle and fitkink cut

--- src/PSFtailCuts.cxx
+++ src/PSFtailCuts.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/merit/src/PSFtailCuts.cxx,v 1.1.1.1 1999/12/20 22:29:13 burnett Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/merit/src/PSFtailCuts.cxx,v 1.2 2001/06/14 19:51:59 usher Exp $
 // PSFtailCuts.cxx: implementation of the PSFtailCuts class.
 //
 //////////////////////////////////////////////////////////////////////
@@ -23,7 +23,78 @@ class AbsValueCut : public Analyze {
         return fabs(item()) < m_value;
     }
     double m_value;
-};   
+};
+
+
+//This function calculates the energy/angle dependent cut for FitKink and Tangle
+static float kalError(float kalEne, int kplane, float cosz) 
+{
+	if (kalEne <0) return 1e6;
+
+    //Convert from MeV to GeV for calculation
+    kalEne *= 0.001;
+
+    //Calculate the cut value depending on top or bottom
+    float kalFit;
+
+    if (kplane  < 12) kalFit = (2.8 /kalEne) + (2.1 /sqrt(kalEne));
+    else              kalFit = kalFit = (6.35/kalEne) + (3.75/sqrt(kalEne));
+
+    kalFit = - (0.001*kalFit) / cosz;
+
+    return 3.5 * kalFit;
+}
+
+
+class FitKinkCut : public Analyze
+{
+    friend class PSFtailCuts;
+
+    FitKinkCut(const Tuple& t) : Analyze(t, "TKR_Fit_Kink", "Tracker Fit Kink"),
+                                 m_CalEne  (t.tupleItem("Cal_Energy_Deposit")),
+                                 m_1stPlane(t.tupleItem("TKR_First_XHit")),
+                                 m_cosTheta(t.tupleItem("TKR_Gamma_zdir"))
+    { }
+
+    virtual bool apply()
+    {
+        float fitKink = item();
+        float calEne  = *m_CalEne;
+        int   frstLyr = *m_1stPlane;
+        float cosZ    = *m_cosTheta;
+
+        return fabs(fitKink) <= kalError(calEne, frstLyr, cosZ);
+    }
+
+    const TupleItem* m_CalEne;
+    const TupleItem* m_1stPlane;
+    const TupleItem* m_cosTheta;
+};
+
+class TAngleCut : public Analyze
+{
+    friend class PSFtailCuts;
+
+    TAngleCut(const Tuple& t) : Analyze(t, "TKR_t_angle", "Tracker t0-t1 angle"),
+                                m_CalEne  (t.tupleItem("Cal_Energy_Deposit")),
+                                m_1stPlane(t.tupleItem("TKR_First_XHit")),
+                                m_cosTheta(t.tupleItem("TKR_Gamma_zdir"))
+    { }
+
+    virtual bool apply()
+    {
+        float tangle  = item();
+        float calEne  = *m_CalEne;
+        int   frstLyr = *m_1stPlane;
+        float cosZ    = *m_cosTheta;
+
+        return fabs(tangle) <= kalError(calEne, frstLyr, cosZ);
+    }
+
+    const TupleItem* m_CalEne;
+    const TupleItem* m_1stPlane;
+    const TupleItem* m_cosTheta;
+};
 
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
@@ -33,10 +104,13 @@ PSFtailCuts::PSFtailCuts(const Tuple& t): AnalysisList(" PSF tail cuts")
 {
 
     push_back( new Cut(t, "TKR_Fit_Type>0") );
-    push_back( new AbsValueCut(t, "TKR_Fit_Kink" , 3*0.0041 ) );
+    //push_back( new AbsValueCut(t, "TKR_Fit_Kink" , 3*0.0041 ) );
    
     // allow either name for the opening angle difference
-    std::string tname="t0-t1_Angle";
-    if (t.find(tname)==t.end()) tname = "TKR_t_angle"; 
-    push_back( new Cut(t, tname, Cut::LT, 4*0.0041 ) );
+    //std::string tname="t0-t1_Angle";
+    //if (t.find(tname)==t.end()) tname = "TKR_t_angle"; 
+    //push_back( new Cut(t, tname, Cut::LT, 4*0.0041 ) );
+
+    push_back( new FitKinkCut(t) );
+    push_back( new TAngleCut(t)  );
 }
