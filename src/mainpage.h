c3c530e0374ad3140280f86494aec58361cb05e9
burnett <>
2002-05-28 03:59:23 +0000

    start of glast-standard doc

---
+++ src/mainpage.h
@@ -0,0 +1,42 @@
+//$Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/mainpage.h,v 1.2 2002/05/11 22:27:59 burnett Exp $
+// (Special "header" just for doxygen)
+
+/*! @mainpage  package merit
+Sample output, the first sensible Gleam-related results!
+<pre>
+meritAlg             INFO 
+        Generated events :   1000
+=======================================================
+Analysis cuts: nA
+          Found in tuple :    211
+-------------------------------------------------------
+  Generated energy--mean :    0.1
+                     rms :      0
+                     min :    0.1
+                     max :    0.1
+         TKR_No_Tracks>0 :    191
+   Accepted for analysis :    191
+-------------------------------------------------------
+ Layers 0-11
+             Events used :    191
+        eff. proj. sigma :   2.61 deg = 156 arc-min
+           68% contained :   4.72 deg =  1.2*(1.51*sigma)
+           95% contained :     17 deg = 2.66*(2.45*sigma)
+        Energy: meas/gen :  0.421
+                     std :  0.199
+       events w/ no data :      1
+          effective area :  11460 cm^2
+         Figure of merit :   2352 cm
+-------------------------------------------------------
+ Layers 12-15
+             Events used :      0
+        Energy: meas/gen :      0
+                     std :      0
+       events w/ no data :      0
+          effective area :      0 cm^2
+         Figure of merit :      0 cm
+-------------------------------------------------------
+    total effective area :  11460 cm^2
+            Combined FOM :      0 cm
+</pre>
+*/
