ec66ea7c99edd3fc171ee15c9b4d363d0751514d
burnett <>
2002-09-01 04:53:49 +0000

    TNtuple sucks!!!! Convert to TTree.

--- src/meritAlg/MeritRootTuple.cxx
+++ src/meritAlg/MeritRootTuple.cxx
@@ -3,69 +3,38 @@
 #include "analysis/Tuple.h"
 
 // root includes
-#include "TNtuple.h"
+#include "TTree.h"
 #include "TFile.h"
 
-#include <vector>
-#include <string>
-#include <cassert>
-
-#ifndef DEFECT_NO_STRINGSTREAM
-# include <sstream>
-#else
-# include <strstream>
-#endif
 
 
-MeritRootTuple::MeritRootTuple(const Tuple* tuple, std::string filename)
+MeritRootTuple::MeritRootTuple(Tuple* tuple, std::string filename)
 : m_tuple(tuple)
 {
-#ifndef DEFECT_NO_STRINGSTREAM
-    std::stringstream list;
-#else
-    std::strstream list;
-#endif
-
-    bool first = true;
-    for(Tuple::const_iterator tit =m_tuple->begin(); tit != m_tuple->end(); ++tit){
-        const TupleItem& item = **tit;
-        if( first ) {  first=false;}else list << ":";
-        list << item.name();
-    }
-    std::string namestring=list.str();
-    const char * names = namestring.c_str();
-    static char buffer[1024];
-    ::strncpy(buffer, names, sizeof(buffer));
 
     m_tf = new TFile(filename.c_str(),"RECREATE");
-    m_tnt = new TNtuple("MeritTuple", m_tuple->title().c_str(), buffer);
-    assert( tuple->size() == m_tnt->GetNvar() ); // must be the same
+
+    m_tree = new TTree("MeritTuple", m_tuple->title().c_str());
+    for(Tuple::iterator tit =m_tuple->begin(); tit != m_tuple->end(); ++tit){
+        TupleItem& item = **tit;
+        m_tree->Branch(item.name().c_str(), &item.value(), item.name().c_str());
+    }
 }
 
 void MeritRootTuple::fill()
 {
-    std::vector<float> floatarray(m_tuple->size());
-
-    std::vector<float>::iterator fit = floatarray.begin();
-
-    for(Tuple::const_iterator tit =m_tuple->begin(); tit != m_tuple->end(); ++tit){
-        const TupleItem& item = **tit;
-        *fit++ = float(item);
-    }
-
-    const Float_t* x = floatarray.begin();
-    m_tnt->Fill(x);
+    m_tree->Fill();
 }
 
 int MeritRootTuple::entries() const { 
-    return m_tnt->GetEntries();
+    return m_tree->GetEntries();
 }
 
 MeritRootTuple::~MeritRootTuple()
 {
-    m_tf->Write();
+    m_tree->Write();
 #if 0 // crashes ROOT???
     delete m_tf;
-    delete m_tnt;
+    delete m_tree;
 #endif
 }
