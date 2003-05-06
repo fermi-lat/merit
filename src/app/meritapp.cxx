3bf935df655171ee29f30e0404d5a90cdb268f8e
burnett <>
2003-05-06 05:08:25 +0000

    Using classification to add to tuple.

--- src/app/meritapp.cxx
+++ src/app/meritapp.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/merit/src/app/meritapp.cxx,v 1.11 2002/10/27 19:28:40 burnett Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/merit/src/app/meritapp.cxx,v 1.12 2003/03/15 17:59:34 burnett Exp $
 
 // Main for merit
 
@@ -8,6 +8,7 @@
 
 #include "../Analyze.h"
 #include "../FigureOfMerit.h"
+#include "../ClassificationTree.h"
 
 #include <iostream>
 #include <fstream>
@@ -18,7 +19,7 @@
 
 #include <assert.h>
 
-const char* _MERIT_VERSION = "$Revision: 1.11 $";
+const char* _MERIT_VERSION = "$Revision: 1.12 $";
 static std::string  cutstr("nA");
 static std::string  file_name("");
 
@@ -56,12 +57,12 @@ static const char* helptext=
 "\n\t\t      b: all bins from 1 to 0.2, 0.1 bins"
 "\n\t\t      n: one bin  from 1 to 0, appropriate for normal incidence, or average"
 "\n\t\t      i: all costh bins but one energy bin, appropriate for single energy"
- ;
+;
 
 void	help()
 {
     std::cerr << "\nmerit Version " << _MERIT_VERSION << " -- Help"
-	   << helptext << "\n\n\tDefault: " << cutstr << std::endl;
+        << helptext << "\n\n\tDefault: " << cutstr << std::endl;
 }
 
 int main(int argc, char* argv[])
@@ -71,115 +72,72 @@ int main(int argc, char* argv[])
     if( argc>++n ) file_name = argv[n];		// required
     if ( std::string(file_name).find("?") != std::string::npos
         || file_name=="--help") {	// print help information
-	help();
-	return 0;
-    }
-    // first arg is option string if preceded by dash
-    if( file_name[0]=='-' && file_name.size()>1) {
-        cutstr = file_name.substr(1);
-        file_name = "";
-    }
-    if( argc>++n ) {			// optional
-        if ( argv[n][0] == 'p' )  {
-            Analyze::showperc(true);
-            n++;
+            help();
+            return 0;
+        }
+        // first arg is option string if preceded by dash
+        if( file_name[0]=='-' && file_name.size()>1) {
+            cutstr = file_name.substr(1);
+            file_name = "";
+        }
+        if( argc>++n ) {			// optional
+            if ( argv[n][0] == 'p' )  {
+                Analyze::showperc(true);
+                n++;
+            }
+
+            if ( argc>n ) {
+                if ( argv[n][0] == '-' )  {	// this is the cut sequence
+                    cutstr = &argv[n][1];
+                } else file_name = argv[n];	
+            }
         }
 
-        if ( argc>n ) {
-            if ( argv[n][0] == '-' )  {	// this is the cut sequence
-                cutstr = &argv[n][1];
-            } else file_name = argv[n];	
+        if( file_name=="" ) {
+            const char * env = ::getenv("MERIT_INPUT_FILE");
+            if( env ) file_name=env;
+            else {
+                std::cerr << "No input file specified, see help" << std::endl;
+                exit(1);
+            }
         }
-    }
 
-    if( file_name=="" ) {
-        const char * env = ::getenv("MERIT_INPUT_FILE");
-        if( env ) file_name=env;
-        else {
-            std::cerr << "No input file specified, see help" << std::endl;
-            exit(1);
+        // setup output stream
+        std::ostream* outstream = &std::cerr;
+        const char * env = ::getenv("MERIT_OUTPUT_FILE");
+        if( env ){
+            outstream = new std::ofstream(env);
         }
-    }
+        std::cerr << "Merit reading from file: \"" << file_name << "\"" << std::endl;
+        // charge a head with ROOT here, figure out later how to do it consistently
 
-	// setup output stream
-	std::ostream* outstream = &std::cerr;
-	const char * env = ::getenv("MERIT_OUTPUT_FILE");
-	if( env ){
-		outstream = new std::ofstream(env);
-	}
-    std::cerr << "Merit reading from file: \"" << file_name << "\"" << std::endl;
-    // charge a head with ROOT here, figure out later how to do it consistently
-
-//    RootTuple* tuple = new RootTuple("unknown", file_name, "AORECON/t1");
-    RootTuple* tuple = new RootTuple("unknown", file_name, "MeritTuple");
-    // now assign input stream, either the file name or std input
+        //    RootTuple* tuple = new RootTuple("unknown", file_name, "AORECON/t1");
+        RootTuple* tuple = new RootTuple("unknown", file_name, "MeritTuple");
+        // now assign input stream, either the file name or std input
         // Determine the # of events stored in ntuple
 
-    std::stringstream title; 
-    title << "gen(" << tuple->numEvents() << ")" << '\0';
-    tuple->setTitle(title.str());
+        std::stringstream title; 
+        title << "gen(" << tuple->numEvents() << ")" << '\0';
+        tuple->setTitle(title.str());
 
-#if 0
-    std::istream* infile;
-    if( file_name== "-") {
-        infile = &std::cin;
-    } else {
-        std::ifstream* realfile = new std::ifstream(file_name.c_str());
-        if( !realfile->is_open()) {
-            std::cerr << "Cannot open file \"" << file_name << "\"";
-            exit(1);
+
+        (*outstream) << "Tuple title: \""<< tuple->title() << "\"\n" ;
+
+        // create the ct: pass in the tuple.
+        ClassificationTree ct(*tuple);
+        FigureOfMerit fm(*tuple, cutstr);
+
+        ::ftime(&t_init);
+
+        while ( tuple->nextEvent() ) { 
+            ct.execute();   // fill in the classification (testing here)
+            fm.execute(); // run the rest.
         }
+        ::ftime(&t_final);
+        fm.report(*outstream);
 
-	infile = realfile;
-    }
-    Tuple* tuple;
-
-    if( file_name.find_first_of(".root") >0 ) {
-        // opened a root file
-        tuple = new RootTuple("unknown title", file_name, "t1");
-    }
-    else {
-        // assume it is ascii. (need to fix, I think).
-        tuple = new AsciiTuple(*infile);
-    }
-
-
-#endif
-    (*outstream) << "Tuple title: \""<< tuple->title() << "\"\n" ;
-
-    FigureOfMerit fm(*tuple, cutstr);
-
-    ::ftime(&t_init);
-
-    while ( tuple->nextEvent() ) fm.execute();
-    ::ftime(&t_final);
-    fm.report(*outstream);
-	
-    std::cerr << "\nElapsed time: "<< t_final.time-t_init.time << " sec" << std::endl;
-    return 0;
+        std::cerr << "\nElapsed time: "<< t_final.time-t_init.time << " sec" << std::endl;
+        return 0;
 }
 
-#if 0 // ascii multi-file here for future reference
-    int line=0, m=1;
-    for(;;) {
 
-        // read and process the current file
-        for(; ; ++line) {
-	    (*infile) >> tup;
-	    if( !infile->good() ) break;
-	    fm.execute();	
-        }
-        if( argc<=++n) break;
-        // setup next file
-        delete infile;
-        file_name = argv[n];
-        std::ifstream* realfile = new std::ifstream(file_name.c_str());
-        if( !realfile->is_open()) {
-            std::cerr << "Cannot open file \"" << file_name << "\"";
-            exit(1);
-        }
-        tup.nextStream(*(infile=realfile));
-        std::cerr << "Tuple# "<< ++m << " \""<< tup.title() << "\"\n" ;
-
-    }
-#endif
