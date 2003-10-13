0d3f70dfd12b0d73ae3f3c4353ead6af14a72487
cohen <>
2003-10-13 06:21:34 +0000

    adding several FT1 variable, taken from the RECO or the MC info: the MC info does not seem to make much sense though, the RECO info relies on first vertex in the list (probably temporary)

--- src/meritAlg/meritAlg.cxx
+++ src/meritAlg/meritAlg.cxx
@@ -1,7 +1,7 @@
 /** @file meritAlg.cxx
     @brief Declaration and implementation of meritAlg
 
- $Header: /nfs/slac/g/glast/ground/cvs/merit/src/meritAlg/meritAlg.cxx,v 1.53 2003/09/29 03:05:19 burnett Exp $
+ $Header: /nfs/slac/g/glast/ground/cvs/merit/src/meritAlg/meritAlg.cxx,v 1.54 2003/10/03 00:21:12 burnett Exp $
 */
 // Include files
 
@@ -21,6 +21,7 @@
 #include "Event/TopLevel/EventModel.h"
 #include "Event/TopLevel/MCEvent.h"
 #include "Event/MonteCarlo/Exposure.h"
+#include "Event/Recon/TkrRecon/TkrVertex.h"
 
 
 #include "AnalysisNtuple/IValsTool.h"
@@ -117,7 +118,7 @@ private:
     StringProperty m_eventTreeName;
     StringProperty m_pointingTreeName;
     StringProperty m_FT1TreeName;
-
+  StringProperty m_primaryType;
 
 };
 
@@ -138,6 +139,7 @@ Algorithm(name, pSvcLocator), m_tuple(0)
     declareProperty("PointingTreeName", m_pointingTreeName="Exposure");
     declareProperty("FT1TreeName", m_FT1TreeName="FT1");
     declareProperty("IM_filename", m_IM_filename="$(CLASSIFICATIONROOT)/xml/PSF_Analysis.xml");
+    declareProperty("PrimaryType", m_primaryType="RECO");
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 StatusCode meritAlg::setupTools() {
@@ -235,7 +237,7 @@ StatusCode meritAlg::initialize() {
     //now make the parallel ROOT tuple--the treename must be set
     if(!m_eventTreeName.value().empty() ){
         // get a pointer to RootTupleSvc 
-        if( (sc = service("RootTupleSvc", m_rootTupleSvc)). isFailure() ) {
+        if( (sc = service("RootTupleSvc", m_rootTupleSvc, true) ). isFailure() ) {
             log << MSG::ERROR << " failed to get the RootTupleSvc" << endreq;
             return sc;
         }
@@ -308,7 +310,19 @@ void meritAlg::setupFT1info(){
     std::string treeName= m_FT1TreeName.value();
     if( treeName.empty()) return;
     std::vector<const char* > names;
-    const char * FT1_names[] = {"energy", "ra", "dec", "theta", "phi", "zenith", "earth_azimuth", "time", "id"};
+    const char * FT1_names[] = {"energy", 
+				"ra", 
+				"dec", 
+				"theta", 
+				"phi", 
+				"zenith_angle", 
+				"earth_azimuth", 
+				"time", 
+				"event_id",
+				"convPointX",
+				"convPointY",
+				"convPointZ",
+				"convLayer"};
     for( int i = 0; i< sizeof(FT1_names)/sizeof(void*); ++i){ names.push_back(FT1_names[i]); }
     
     m_FT1tuple = new TTree( m_rootTupleSvc,  std::string(m_FT1TreeName),  names);
@@ -318,51 +332,137 @@ void meritAlg::copyFT1info(){
 
     using namespace astro;
 
+    MsgStream log(msgSvc(), name());
+
+    double energy;
+    Hep3Vector convPoint;
+    // "glastdir" is the predicted direction to the source, in instrument coords
+    Hep3Vector glastDir;
+    int convLayer;
+
+    if(m_primaryType.value() == "MC")
+      {
+	// Temporary: get the MC direction here for verification
+	// Recover MC Pointer
+	SmartDataPtr<Event::McParticleCol> pMcParticle(eventSvc(), EventModel::MC::McParticleCol);
+	Event::McParticleCol::const_iterator pMCPrimary = pMcParticle->begin();
+	// Skip the first particle... it's for bookkeeping.
+	// The second particle is the first real propagating particle.
+	pMCPrimary++;
+	HepLorentzVector Mc_p0 = (*pMCPrimary)->initialFourMomentum();
+	
+	energy    = Mc_p0.t(); 
+	glastDir  = - Mc_p0.vect().unit();
+	convPoint = (*pMCPrimary)->initialPosition();
+	convLayer = 0;
+      }
+    else if(m_primaryType.value() == "RECO")
+      {
+	// Retrieve Vertex to get summary info from reco
+	Event::TkrVertexCol* pVtxCol = 
+	  SmartDataPtr<Event::TkrVertexCol>(eventSvc(), EventModel::TkrRecon::TkrVertexCol);
+	if(!pVtxCol)
+	  {
+	    log << MSG::ERROR << "Vertex Col not found in TDS" << endreq;
+	    return;
+	  }
+	if(pVtxCol->size()==0) 
+	  {
+	    log << MSG::ERROR << "Vertex Col found in TDS BUT empty" << endreq;
+	    return;
+	  }
+	//Assuming the best vertex comes always first.....    
+	Event::TkrVertex* pTheBestVtx = pVtxCol->front();
+	
+	energy    = pTheBestVtx->getEnergy();
+	glastDir  = - pTheBestVtx->getDirection().unit();
+	convPoint = pTheBestVtx->getPosition();
+	convLayer = pTheBestVtx->getLayer();
+      }
+
+
+    //Get Event_ID
+    Event::EventHeader* pEvent = 
+      SmartDataPtr<Event::EventHeader>(eventSvc(), EventModel::EventHeader);
+    if(!pEvent)
+      {
+	log << MSG::ERROR << "EventHeader not found in TDS" << endreq;
+	return;
+      }
+    long event_id = pEvent->event();
+
+    //Now we get the coordinates: frol the ExposureCol
     Event::ExposureCol* elist = 0;
     eventSvc()->retrieveObject("/Event/MC/ExposureCol",(DataObject *&)elist);
     if( elist==0) return; // should not happen, but make sure ok.
     const Event::Exposure& exp = **(*elist).begin();
 
     // create a transformation object -- first get local directions
-   SkyDir zsky(exp.RAZ(), exp.DECZ(), SkyDir::CELESTIAL);
-   SkyDir xsky(exp.RAX(), exp.DECX(), SkyDir::CELESTIAL );
-
+    SkyDir zsky(exp.RAZ(), exp.DECZ(), SkyDir::CELESTIAL);
+    SkyDir xsky(exp.RAX(), exp.DECX(), SkyDir::CELESTIAL );
     // orthogonalize, since interpolation and transformations destory orthogonality (limit is 10E-8)
     Hep3Vector xhat = xsky() -  xsky().dot(zsky()) * zsky() ;
     PointingTransform toSky( zsky, xhat );
-
     // make zenith (except for oblateness correction) unit vector
     Hep3Vector zenith( exp.posX(),  exp.posY(), exp.posZ());
     zenith = zenith.unit();
     
 
-    // Temporary: get the MC direction here for verification
-    // Recover MC Pointer
-    SmartDataPtr<Event::McParticleCol> pMcParticle(eventSvc(), EventModel::MC::McParticleCol);
-    Event::McParticleCol::const_iterator pMCPrimary = pMcParticle->begin();
-    // Skip the first particle... it's for bookkeeping.
-    // The second particle is the first real propagating particle.
-    pMCPrimary++;
-    HepLorentzVector Mc_p0 = (*pMCPrimary)->initialFourMomentum();
-    Hep3Vector glastdir=-Mc_p0.vect().unit();   
+    SkyDir sdir = toSky.gDir(glastDir);
 
-    // "glastdir" is the predicted direction to the source, in instrument coords
-    SkyDir sdir = toSky.gDir(glastdir);
+    //zenith_theta and earth_azimuth in degree:
+    double zenith_theta = acos(zenith.dot(sdir()));
+    Hep3Vector north(0.,0.,1.);
+    Hep3Vector planar_dir   = sdir() - (zenith.dot(sdir()))*zenith;
+    Hep3Vector planar_north = sdir() - (zenith.dot(north))*zenith;
+    Hep3Vector planar_east  = planar_north.rotate(M_PI/2.,zenith);
+    planar_dir   = planar_dir.unit();
+    planar_north = planar_north.unit();
+    planar_east  = planar_east.unit();
+
+    Hep3Vector north_to_east = planar_north.cross(planar_east);
+    double biplan = planar_north.dot(planar_dir);
+    double sine_sign = (planar_north.cross(planar_dir).unit() == north_to_east)?1.0:-1.0;
+    double earth_azimuth = planar_north.cross(planar_dir).mag();
+    if ( biplan < 0 && sine_sign > 0) //2nd quadrant
+      {
+	earth_azimuth += M_PI/2.;
+      }
+    else if (biplan < 0 && sine_sign < 0)//3rd quadrant
+      {
+	earth_azimuth += M_PI;
+      }
+    else if (biplan > 0 && sine_sign < 0)//4th quadrant
+      {
+	earth_azimuth += 3.*M_PI/2.;
+      }
 
     // celestial coords
+    m_FT1tuple->fill(0,  energy);
     m_FT1tuple->fill(1,  sdir.ra() ) ;
     m_FT1tuple->fill(2,  sdir.dec() );
 
     // instrument coords
-    m_FT1tuple->fill(3,  glastdir.theta()* 180./M_PI );
-    double phi_deg = glastdir.phi() * 180./M_PI; if( phi_deg<0) phi_deg+=360;
+    m_FT1tuple->fill(3,  glastDir.theta() );
+
+    double phi_deg = glastDir.phi(); 
+    if( phi_deg<0 ) phi_deg += 2*M_PI;
     m_FT1tuple->fill(4,  phi_deg);
 
     // zenith-based coords
-    m_FT1tuple->fill(5, acos(zenith.dot(sdir())) * 180/M_PI);
+    m_FT1tuple->fill(5, zenith_theta);
+    m_FT1tuple->fill(6, earth_azimuth);
 
     // time
     m_FT1tuple->fill(7,  exp.intrvalstart() );
+
+    // event_id
+    m_FT1tuple->fill(8,  event_id );
+
+    m_FT1tuple->fill(9,   convPoint.x() );
+    m_FT1tuple->fill(10,  convPoint.y() );
+    m_FT1tuple->fill(11,  convPoint.z() );
+    m_FT1tuple->fill(12,  convLayer     );
 }
 //------------------------------------------------------------------------------
 void meritAlg::calculate(){
@@ -373,7 +473,7 @@ void meritAlg::calculate(){
 }
 //------------------------------------------------------------------------------
 void meritAlg::printOn(std::ostream& out)const{
-    out << "Merit tuple, " << "$Revision: 1.53 $" << std::endl;
+    out << "Merit tuple, " << "$Revision: 1.54 $" << std::endl;
 
     for(Tuple::const_iterator tit =m_tuple->begin(); tit != m_tuple->end(); ++tit){
         const TupleItem& item = **tit;
