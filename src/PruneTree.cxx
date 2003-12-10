965d2fa6989e11e68239970469aff7be03d5991a
burnett <>
2003-12-10 04:41:48 +0000

    all ready except for preclassification logic

--- src/PruneTree.cxx
+++ src/PruneTree.cxx
@@ -2,11 +2,90 @@
 @brief apply the set of Bill's cuts for pruning background 
 */
 #include "PruneTree.h"
-
 #include "analysis/Tuple.h"
 
-#include <sstream>
 #include <cassert>
+/* 
+Loading Trees from file "D:\Users\burnett\dev_197\merit\v6r14p5/xml/CTPruner_DC1.imw"
+176                    Tiles&Hi-E       177           NoTiles&LowE&GoodXR       179                NoTiles&1Xtals
+180                NoTiles&0Xtals       181        Tiles&ACD< -170&GoodXR       182          Tiles&ACD<-170&1Xtal
+183          Tiles&ACD<-170&0Xtal       184           Tiles&ACD<-20&Med-E       185           Tiles&ACD>-20&Low-E
+Found 9 prediction nodes
+
+Combined used names:
+
+"AcdActiveDist", "AcdDoca", "AcdTotalEnergy", "AcdUpperTileCnt", "CalBkHalfRatio", "CalCsIRLn", "CalDeadTotRat", "CalDel
+taT", "CalEnergySum", "CalLATEdge", "CalLRmsRatio", "CalLyr0Ratio", "CalLyr7Ratio", "CalMIPDiff", "CalTotRLn", "CalTotSu
+mCorr", "CalTrackDoca", "CalTrackSep", "CalTwrEdge", "CalTwrGap", "CalXtalRatio", "CalXtalsTrunc", "EvtCalETLRatio", "Ev
+tCalETrackDoca", "EvtCalETrackSep", "EvtCalEXtalRatio", "EvtCalEXtalTrunc", "EvtCalEdgeAngle", "EvtEnergySumOpt", "EvtLo
+gESum", "EvtTkr1EChisq", "EvtTkr1EFirstChisq", "EvtTkr1EFrac", "EvtTkr1EQual", "EvtTkr1PSFMdRat", "EvtTkr2EChisq", "EvtT
+kr2EFirstChisq", "EvtTkrComptonRatio", "EvtTkrEComptonRatio", "EvtTkrEdgeAngle", "EvtVtxEAngle", "EvtVtxEDoca", "EvtVtxE
+EAngle", "EvtVtxEHeadSep", "Pr(CORE)", "Tkr1Chisq", "Tkr1DieEdge", "Tkr1FirstChisq", "Tkr1FirstLayer", "Tkr1Hits", "Tkr1
+KalEne", "Tkr1PrjTwrEdge", "Tkr1Qual", "Tkr1TwrEdge", "Tkr1ZDir", "Tkr2Chisq", "Tkr2KalEne", "TkrBlankHits", "TkrEnergy"
+, "TkrHDCount", "TkrNumTracks", "TkrRadLength", "TkrSumKalEne", "TkrThickHits", "TkrThinHits", "TkrTotalHits", "TkrTrack
+Length", "TkrTwrEdge", "VtxAngle", "VtxDOCA", "VtxHeadSep", "VtxS1", "VtxTotalWgt", "VtxZDir",
+
+Following names were needed by nodes, but not supplied in the map:
+
+"AcdUpperTileCnt",
+
+*/
+// need to add: AcdUpperTileCnt=AcdTileCount-AcdNoSideRow2 -AcdNoSideRow1
+
+namespace {
+
+    // Convenient identifiers used for the nodes
+    typedef enum{ ONE, TWO, THREE,  FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, 
+       NODE_COUNT, NONE
+    } Category;
+
+    /** table of information about nodes to expect in the IM file.
+
+    */
+    class IMnodeInfo { 
+    public:
+        int id;           // unique ID for local identification
+        const char* name; // the name of the Preciction node in the IM XML file
+        int index;        // index of the classification type within the list of probabilites
+    };
+    // these have to correspond to the IM file,
+    IMnodeInfo imNodeInfo[] = {
+        { ONE,  "Tiles&Hi-E",  1 },
+        { TWO,  "NoTiles&LowE&GoodXR",   1 }, 
+        { THREE,"NoTiles&1Xtals",   1 },
+        { FOUR, "NoTiles&0Xtals",    1 },
+        { FIVE,  "Tiles&ACD< -170&GoodXR",     1},
+        { SIX,     "Tiles&ACD<-170&1Xtal",     0 },
+        { SEVEN,   "Tiles&ACD<-170&0Xtal" ,1}, 
+        { EIGHT,   "Tiles&ACD<-20&Med-E", 0},
+        { NINE,     "Tiles&ACD>-20&Low-E",1},
+      
+    };
+
+    /** Manage interface to one of the prediction nodes
+
+    */
+    class IMpredictNode { 
+    public:
+        IMpredictNode(const IMnodeInfo& info, const classification::Tree* tree)
+            :  m_offset(info.index), m_tree(tree)
+        {
+            m_node = m_tree->getPredictTree(info.name);
+            if( m_node==0) std::cerr << "Tree " << info.name << " not found in tree" << std::endl;
+            assert(m_node);
+        }
+
+        double evaluate() const{
+            return m_tree->navigate(m_node)[m_offset];
+        }
+        int m_offset;
+        const classification::Tree* m_tree;
+        const classification::Tree::Node* m_node;
+    };
+
+  std::vector<IMpredictNode> imnodes;
+
+} //anonymous namespace
 
 // create lookup class to make translations
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -28,34 +107,49 @@ class PruneTree::PreClassify {
 public:
     //! ctor: just associate with tree and find the branches we need
     PreClassify(PruneTree::Lookup& lookup) 
-        : VtxAngle (*lookup(                 "VtxAngle")) 
-        , EvtEnergySumOpt (*lookup(      "EvtEnergySumOpt"))
-        , EvtTkrComptonRatio (*lookup("EvtTkrComptonRatio")) 
-        , Tkr1ToTFirst (*lookup(             "Tkr1ToTFirst"))
-        , Tkr1ToTAve (*lookup(             "Tkr1ToTAve"))
-        , AcdTotalEnergy (*lookup(        "AcdTotalEnergy"))  
-        , AcdRibbonActDist (*lookup(    "AcdRibbonActDist")) 
-        , AcdTileCount (*lookup(          "AcdTileCount"))
-        , FilterStatus_HI (*lookup(         "FilterStatus_HI"))
-    {  }
+        : CalEnergySum (*lookup(     "CalEnergySum")) 
+        , CalXtalRatio (*lookup(        "CalXtalRatio"))
+        , CalXtalsTrunc (*lookup(     "CalXtalsTrunc")) 
+        , AcdActiveDist (*lookup(     "AcdActiveDist"))
+        , AcdTileCount (*lookup(      "AcdTileCount"))
+        , AcdNoSideRow2 (*lookup(  "AcdNoSideRow2"))
+        , AcdNoSideRow1 (*lookup(  "AcdNoSideRow1"))
+    { 
+        // need a new tuple item
+        new TupleItem("AcdUpperTileCnt", AcdUpperTileCnt);
+    }
 
-    typedef  enum {NONE, ONE, TWO}  Category;
     //! return truth value, for current TTree position
     operator Category()
     {
+        AcdUpperTileCnt=AcdTileCount-AcdNoSideRow2 -AcdNoSideRow1;
+        // todo: apply pre-categorization logic
+        /*
+        CalEnergySum > 5 & CalCsIRLn > 2
+(AcdActiveDist < -20 & AcdRibbonActDist < -20) | Tkr1SSDVeto > 1
+
+MeasEnergyType	categorical	ifelse((CalEnergySum < 350),"Low-E",ifelse((CalEnergySum < 3500),"Med-E","Hi-E"))
+MeasEnergyType == "Hi-E"
+AcdTileCount == 0
+CalXtalRatio > .02 & CalXtalRatio < .98
+CalXtalsTrunc > 0
+AcdActiveDist <-150
+CalXtalRatio > .02 & CalXtalRatio < .98
+AcdActiveDist < -20 &MeasEnergyType == "Med-E"
+*/
         return NONE;
     }
 private:
     // references to the values read in from ROOT, or set directly, in the tuple
-    const double &   VtxAngle; 
-    const double &   EvtEnergySumOpt;
-    const double &   EvtTkrComptonRatio;
-    const double &   Tkr1ToTFirst;
-    const double &   Tkr1ToTAve;
-    const double &   AcdTotalEnergy;  
-    const double &   AcdRibbonActDist; 
+    const double &   CalEnergySum; 
+    const double &   CalXtalRatio;
+    const double &   CalXtalsTrunc;
+    const double &   AcdActiveDist; 
     const double &   AcdTileCount;
-    const double &   FilterStatus_HI;
+    const double &   AcdNoSideRow1 ;
+    const double &   AcdNoSideRow2 ;
+    // output
+    double  AcdUpperTileCnt;
 };
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -67,19 +161,30 @@ PruneTree::PruneTree( Tuple& t, std::ostream& log, std::string xml_file)
     m_classifier = new classification::Tree(looker, log, 0); // verbosity
     // translate the Tuple map
 
+    std::string default_file("/xml/CTPruner_DC1.imw");
+
+    if( xml_file.empty() ){
+        const char *sPath = ::getenv("MERITROOT");
+        xml_file= std::string(sPath==0?  "../": sPath) + default_file;
+    }
     m_classifier->load(xml_file);
 
-    // TODO: get the list of root prediction tree nodes
+    // get the list of root prediction tree nodes
+    imnodes.reserve(NODE_COUNT);
+    for( unsigned int i=0; i<NODE_COUNT; ++i){
+        IMpredictNode n(imNodeInfo[i],m_classifier);
+        imnodes[imNodeInfo[i].id]=n;
+    }
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 PruneTree::~PruneTree(){
     delete m_preclassify;
     delete m_classifier;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void PruneTree::execute()
 {
-    PreClassify::Category  test = *m_preclassify;
+   Category  cat = *m_preclassify;
+   double gammaprob= (cat==NONE) ?  0 :  imnodes[cat].evaluate();
+    //TODO: save this in the tuple
 }
