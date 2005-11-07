10584dd5264a1b0b49a39dfcb37426099319d631
burnett <>
2005-11-07 04:27:00 +0000

    new FT1Alg (refactor Johann's code form meritAlg) changed classification code

--- src/Dll/merit_load.cxx
+++ src/Dll/merit_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/merit/src/Dll/merit_load.cxx,v 1.2 2002/05/26 03:35:09 burnett Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/merit/src/Dll/merit_load.cxx,v 1.3 2005/07/29 01:10:23 burnett Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -11,6 +11,7 @@
 DECLARE_FACTORY_ENTRIES(merit) {
     DECLARE_ALGORITHM( meritAlg );
 
+    DECLARE_ALGORITHM( FT1Alg );
 } 
 
 
