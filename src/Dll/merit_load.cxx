32b92ad5718e3d7fffc3b79bd769805132a190cb
burnett <>
2006-10-23 19:04:55 +0000

    remove ClassifyAlg

--- src/Dll/merit_load.cxx
+++ src/Dll/merit_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/merit/src/Dll/merit_load.cxx,v 1.4 2005/11/07 04:27:00 burnett Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/merit/src/Dll/merit_load.cxx,v 1.5 2005/11/08 15:47:38 burnett Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -12,7 +12,6 @@ DECLARE_FACTORY_ENTRIES(merit) {
     DECLARE_ALGORITHM( meritAlg );
 
     DECLARE_ALGORITHM( FT1Alg );
-    DECLARE_ALGORITHM( ClassifyAlg);
 } 
 
 
