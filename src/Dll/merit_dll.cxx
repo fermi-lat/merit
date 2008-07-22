c8c0b3aad7c0860af80872814ef9ab4cc7de5f62
cvs2git <>
2008-07-22 18:38:05 +0000

    This commit was manufactured by cvs2svn to create tag 'v6r36p1'.
    Sprout from master 2008-07-22 18:38:04 UTC lsrea 'Added new line - version v6r36p1'
    Delete:
        src/AnalysisList.cxx
        src/AnalysisList.h
        src/Analyze.cxx
        src/Analyze.h
        src/BackgroundCuts.cxx
        src/BackgroundCuts.h
        src/Cut.cxx
        src/Cut.h
        src/Dll/merit_dll.cxx
        src/Dll/merit_load.cxx
        src/EnergyAnalysis.cxx
        src/EnergyAnalysis.h
        src/FigureOfMerit.cxx
        src/FigureOfMerit.h
        src/LayerGroup.cxx
        src/LayerGroup.h
        src/Level1.cxx
        src/Level1.h
        src/MultiPSF.cxx
        src/MultiPSF.h
        src/PSFRootFit.cxx
        src/PSFRootFit.h
        src/PSFanalysis.cxx
        src/PSFanalysis.h
        src/PSFtailCuts.cxx
        src/PSFtailCuts.h
        src/Statistic.cxx
        src/Statistic.h
        src/analysis/Histogram.cxx
        src/analysis/Histogram.h
        src/analysis/RebinHist.cxx
        src/analysis/RebinHist.h
        src/analysis/Tuple.cxx
        src/analysis/Tuple.h
        src/analysis/TupleTitle.cxx
        src/analysis/TupleTitle.h
        src/analysis/smplstat.cxx
        src/analysis/smplstat.h
        src/app/AsciiTuple.cxx
        src/app/AsciiTuple.h
        src/app/RootTuple.cxx
        src/app/RootTuple.h
        src/app/fastFilter.cxx
        src/app/meritapp.cxx
        src/app/run_classification_main.cxx
        src/app/write_text_main.cxx
        src/mainpage.h
        src/meritAlg/meritAlg.cxx
        src/test/merit100.root

--- src/Dll/merit_dll.cxx
+++
@@ -1,9 +0,0 @@
-/** @file merit_dll.cxx
- @brief Gaudi interface
-
-  $Header: /nfs/slac/g/glast/ground/cvs/merit/src/Dll/merit_dll.cxx,v 1.2 2002/05/26 03:35:09 burnett Exp $
-*/
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-LOAD_FACTORY_ENTRIES(merit)
-
