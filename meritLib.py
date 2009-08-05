4fc69d2dbdb93af0371290d58f4073b320197f66
jrb <>
2009-08-05 23:04:20 +0000

    Value for 'library' arg of addLibrary must be a list

--- meritLib.py
+++ meritLib.py
@@ -1,7 +1,7 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/merit/meritLib.py,v 1.1 2008/08/15 21:42:39 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = 'merit')
+        env.Tool('addLibrary', library = ['merit'])
 
     env.Tool('addLibrary', library = env['rootLibs'])
 
